e43368ff1e0ce2f28d68ffac20fb5f39d85ec50c
Leppy-oss <70586381+Leppy-oss@users.noreply.github.com>
2023-03-05 19:02:30 -0600

    got the slide rendering to work, working on world bounds collision and dual-body htiboxing

--- src/obj/Robot.js
+++ src/obj/Robot.js
@@ -10,7 +10,7 @@ import { angledTranslation, inchesToGamePixels } from "../utils";
  * @param {Number} speedCap Maximum velocity of the this
  * @param {Number} mass Mass of the this; used to calculate matter calculations and collision interactions
  */
-export default function Robot(index, game, alliance, cones = 0, x = GameDimensions[0] / 2, y = GameDimensions[1] / 2, width = 14, height = 14, slideWidth = 48, slideHeight = 7, acc = 1000, friction = 0.8, speedCap = 2000, mass = 10000) {
+export default function Robot(index, game, alliance, cones = 0, x = GameDimensions[0] / 2, y = GameDimensions[1] / 2, width = 14, height = 14, slideWidth = 48, slideHeight = 7, acc = 1000, friction = 0.8, speedCap = 2000, mass = 1000) {
     this.x = x;
     this.y = y;
     this.cones = cones;
@@ -29,17 +29,28 @@ export default function Robot(index, game, alliance, cones = 0, x = GameDimensio
     this.slidePos = this.retractedPos;
     this.slideTargetPos = this.retractedPos;
     this.slideCollidingWithSomething = false;
-    this.slide = game.matter.add.sprite(this.x, this.y, 'linearslide').setSize(this.slideWidth, this.slideHeight).setDisplaySize(this.slideWidth, this.slideHeight);
+    this.slide = this.game.matter.add.sprite(this.x, this.y, 'linearslide').setSize(this.slideWidth, this.slideHeight).setDisplaySize(this.slideWidth, this.slideHeight);
     // this.slide.setCollideWorldBounds(true);
-    this.chassis = game.matter.add.sprite(this.x, this.y, this.alliance == 'RED' ? 'redlight' : 'bluelight');
+    this.chassis = this.game.matter.add.sprite(this.x, this.y, this.alliance == 'RED' ? 'redlight' : 'bluelight');
+    let walls = this.game.matter.add.rectangle(0, 0, GameDimensions[0], GameDimensions[1] * 2);
+    walls.isStatic = true;
+    walls.angularVelocity = 10;
+    walls.collisionFilter.group = 1;
+    walls.collisionFilter.category = 1;
+    walls.collisionFilter.mask = 1;
     this.slide.setCollisionGroup(1); // robtob
-    this.chassis.setCollisionGroup(2); // robtob
-    this.slide.setCollidesWith([0]); // environment
-    this.chassis.setCollidesWith([0]); // environment
-    this.chassis.setSize(this.width, this.height);
+    this.slide.setCollisionCategory(0); // robtob
+    this.slide.setCollidesWith(1); // environment
+    this.slide.setCollisionCategory(0); // robtob
+    this.chassis.setCollisionGroup(1); // robtob
+    this.chassis.setCollisionCategory(0); // robtob
+    this.chassis.setCollidesWith(1); // environment
+    // this.chassis.setCollisionCategory(1); // robtob
+
+
     this.chassis.setDisplaySize(this.width, this.height);
     this.chassis.setMass(this.mass);
-    // this.chassis.setFrictionAir(this.friction / 100);
+    this.chassis.setFrictionAir(this.friction / 10);
     this.chassis.setFriction(this.friction);
     this.chassis.setFrictionStatic(this.friction);
     // this.chassis.setSize(this.width, this.height);
@@ -62,15 +73,17 @@ Robot.prototype.damage = function() {
 }
 
 Robot.prototype.update = function() {
-    this.slidePos = new Phaser.Math.Vector2(this.slidePos, 0).lerp(new Phaser.Math.Vector2(this.slideTargetPos, 0), 0.05).x;
-
+    this.slidePos = new Phaser.Math.Vector2(this.slidePos, 0).lerp(new Phaser.Math.Vector2(this.slideTargetPos, 0), 0.1).x;
     this.slide.setDisplaySize(this.slidePos * 2, this.slideHeight); // true argument updates the body size
     this.slide.setBody({
+        x: this.slide.x,
+        y: this.slide.y,
         width: this.slidePos * 2,
         height: this.slideHeight,
-        sides: 4
+        sides: 4,
     });
     this.slide.setRotation(this.chassis.rotation - Math.PI / 2);
+    this.slide.setTexture('linearslide');
     let tempCoords = angledTranslation(this.slidePos, this.chassis.x, this.chassis.y, this.slide.rotation);
     this.slide.setPosition(tempCoords[0], tempCoords[1]);
 };
